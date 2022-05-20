fed4b705aaffd2ef8f20d5e7739a67d6e07ac84b
Veromadinina <verotldream@gmail.com>
2022-05-20 09:28:05 -0400

    first commit

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Créer un post') }}
+        </h2>
+    </x-slot>
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+        <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data" class="mt-10">
+
+            <x-label for="title" value="Titre du post"/>
+            <x-input id="title" name="title"/>
+
+            <x-label for="content" value="Contenu du post"/>
+            <textarea id="content" name="content"></textarea>
+
+            <x-label for="title" value="Titre du post"/>
+            <x-input id="title" name="title"/>
+
+            
+            
+
+        </form>
+     
+    </div>
+
+
+</x-app-layout>
