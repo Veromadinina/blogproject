1567183a9e8edce4ac346465b6aacdf0e58d4785
Veromadinina <verotldream@gmail.com>
2022-05-20 09:28:19 -0400

    add navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,10 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
-            </div>
-            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <x-nav-link :href="route('post.create')" :active="request()->routeIs('dashboard')">
+                     <x-nav-link :href="route('posts.create')" :active="request()->routeIs('dashboard')">
                     {{ __('Créer un post') }}
                 </x-nav-link>
             </div>
